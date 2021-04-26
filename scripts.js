04bc1e34eb0acb7dc3aa2ad5709716e100f1090e
Sergio <alonsodecaso.sergio@gmail.com>
2021-04-26 18:34:53 +0200

    Ejercicios 2 realizado junto a la funcion palindromo

--- scripts.js
+++ scripts.js
@@ -11,3 +11,36 @@ function parImpar() {
 
     alert("El numero "+ numero  +" es "+resultado);
 }
+
+function palindromo(){
+
+    var frase = prompt("Introduzca una frase");
+
+    var cadenaMinus = frase.toLowerCase();
+    var cadenaSinEspacios = cadenaMinus.split("");
+
+    var cadena = "";
+    for(i in cadenaSinEspacios) {
+
+        if(cadenaSinEspacios[i] != " ") {
+            cadena += cadenaSinEspacios[i];
+        }
+    }
+
+    var cadenaReves = cadena.split("").reverse();
+
+    var palindromo = true;
+
+    for(i in cadena){
+
+        if(cadenaReves[i] != cadena[i]){
+            palindromo = false;
+        }
+    }
+
+    if(palindromo){
+        alert("La frase: " + frase + " es un palindromo ");
+    }else{
+        alert("La frase: " + frase + " NO es un palindromo");
+    }
+}
